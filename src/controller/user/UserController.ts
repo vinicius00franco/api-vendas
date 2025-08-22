57f316287b57d904605e1d7c97fe0a6bd23c5560
vinicius00franco <vincus.franco.silva@gmail.com>
2025-08-21 21:50:32 -0300

    Fix typo in 'email' field to 'emalil' in UserController and HTTP requests

--- src/controller/user/UserController.ts
+++ src/controller/user/UserController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 
 class UserController {
   async create(request: Request, response: Response) {
-    const { name, email, admin, password } = request.body;
-    const user = { name, email, admin, password };
+    const { name, emalil, admin, password } = request.body;
+    const user = { name, emalil, admin, password };
     response.json({ message: "Usuário incluído com sucesso" });
   }
 
   async update(request: Request, response: Response) {
     const { id } = request.params;
-    const { name, email, admin, password } = request.body;
+    const { name, emalil, admin, password } = request.body;
     response.json({ message: "Usuário atualizado com sucesso", id });
   }
 
